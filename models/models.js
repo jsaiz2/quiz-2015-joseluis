f293b9019fc91403c9b48c56e7ee406902a67082
jsaiz2 <jsaiz2@gmail.com>
2015-06-09 11:18:51 +0200

    Paso 9
    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -30,15 +30,18 @@ var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz; // exportar tabla Quiz
 
-// sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
-	// success(..) ejecuta el manejador una vez creada la tabla cambiar por then
-	Quiz.count().success(function (count){
+// sequelize.sync() inicializa tabla de preguntas en DB
+sequelize.sync().then(function() {
+	// then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function (count){
 		if (count === 0) {	//la tabla se inicializa solo si está vacía
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
+						});
+			Quiz.create({  pregunta: 'Capital de Portugal',
+						   respuesta: 'Lisboa'
 						})
-			.success(function(){console.log('Base de datos iniciada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
